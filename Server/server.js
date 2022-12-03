ae8b152fa569ca77be07a64186d5ae99da985430
Aditya Prakash <adityajaiswal735@gmail.com>
2022-12-04 00:24:32 +0530

    jj

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path=require("path");
 const mongoose=require("mongoose");
 const app = express();
 const dotenv=require("dotenv");
@@ -12,11 +13,12 @@ const User=require('./Schema/userSchema');
 app.use(express.json());
 app.use(cookieparser());
 app.use(require('./routes/authentication'));
+app.use(express.static(path.join(__dirname,'./clint/build')));
 
-if(process.env.NODE_ENV == "production")
-{
-    app.use(express.static("clint/build"));
-}
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname,"./clint/build/index.html"));
+});
+
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
